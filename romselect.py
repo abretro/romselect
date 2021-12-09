a41d3fa88d8cbe4b01574efd61d4dae543f885dd
krt <abeckit.github@falcon.org>
2021-12-09 01:28:23 -0800

    conversion to 3 + bug fix

--- romselect.py
+++ romselect.py
@@ -1,64 +1,7 @@
-#!/usr/bin/env python
-
-
+#!/usr/bin/env python3
 
 """
-TODO:
-
-- loop in the menu so multiple roms can be picked
-
-- multiple roms at once (1,2,3)
-
-- drive new system aliasing, like pcengine = turbografx16, etc.
-  (rom aliasing is implemented, but probably not completely defined)
-
-- ansi menus
-
-- only run if there is more than N roms in archive mode (for processing existing
-directories)
-
-- fix pathing so that it can handle various references (./rom.zip, etc.)
-  (workaround is to use full paths)
-
-
-"""
-
-"""
-# Why I didnt use python-libarchive:
-#
-# it depends on libarchive:
-# apt-get install build-essential libtool python-dev automake
-# cd ~/Downloads
-# wget https://libarchive.org/downloads/libarchive-3.3.3.tar.gz
-# cd /usr/src
-# tar xzvpf ~/Downloads/libarchive-3.3.3.tar.gz
-# cd libarchive-3.3.3
-# build/autogen.sh
-# ./configure --prefix=/usr/local
-# make
-# make install
-#
-# echo /usr/local/lib > /etc/ld.so.conf.d/libarchive3.conf
-# ldconfig -v
-#
-# And then actually getting python-libarchive...
-#
-# cd ~/Downloads
-# wget https://storage.googleapis.com/google-code-archive-downloads/\
-# v2/code.google.com/python-libarchive/python-libarchive-3.1.2-1.tar.gz
-#
-# cd /usr/src
-# tar xzpvf ~/Downloads/python-libarchive-3.1.2-1.tar.gz
-# cd python-libarchive-3.1.2-1
-# python setup.py
-#
-# where the versions in raspbian stock are so old
-# that the documentation doesnt reflect them in
-# any usable manner.
-#
-#
-# oh queso, maybe not libarchive.
-#
+romselect.py for retropie            
 """
 import os
 import re
@@ -68,51 +11,62 @@ import sys
 import time
 
 from datetime import datetime
+from pathlib import Path
 
-WORK_DIR = '/home/pi/work'
+# where compression/decompression files are staged:
+WORK_DIR = '/tmp/work'
 
+# where to copy the roms to (romselect knows over 15 rom types at the moment)
 ROMS_DIR = '/home/pi/RetroPie/roms'
 
-#'file extension': 'RetroPie/roms/directory'
+# what flags represent the "good" roms (set in the filename of the rom):
+ROM_GOOD = '[!]'
+
+# what country code is default? (set in the filename of the rom):
+ROM_COUNTRY = 'U'
+
+
 
 """
-~/RetroPie/roms:
-amstradcpc  atarilynx  gbc            n64       ports    ti99
-arcade      fba        genesis        neogeo    psx      vectrex
-atari2600   fds        mame-libretro  nes       sega32x  zxspectrum
-atari5200   gamegear   mame-mame4all  ngp       segacd
-atari7800   gb         mastersystem   ngpc      sg-1000
-atari800    gba        megadrive      pcengine  snes
+# not all of these platforms are currently coded to work with romselect.
+# (PR's will be looked at)
+~/RetroPie/roms $ ls
+amstradcpc  atarijaguar  gamegear   mame-advmame   nds     pcengine  snes
+arcade      atarilynx    gb         mame-libretro  neogeo  ports     ti99
+atari2600   atarist      gba        mame-mame4all  nes     psx       ti99sim
+atari5200   dreamcast    gbc        mastersystem   ngp     sega32x   vectrex
+atari7800   fba          genesis    megadrive      ngpc    segacd    x68000
+atari800    fds          macintosh  n64            pc      sg-1000   zxspectrum
+
+
+Specific platform notes:
+
+
+neogeo and the cdrom platforms:
+    All of these use .bin, so there is no default platform for .bin
+
 """
 
 EXTENSIONS = { 
+    # regular set
+    'a26': 'atari2600',
+    'a78': 'atari7800',
+    'lnx': 'atarilynx',
+    'gen': 'megadrive',
+    'gg': 'gamegear',
+    'pce': 'pcengine',
+    'sfc': 'snes',
+    'smc': 'snes',
+    'sms': 'mastersystem',
 
-# needs to handle the 5 supergrafx roms special since they're .pce too
-# neogeo are .bin, ugh.
-
-# regular set
-'a26': 'atari2600',
-'a78': 'atari7800',
-'lnx': 'atarilynx',
-'gen': 'genesis',
-'gg': 'gamegear',
-'pce': 'pcengine',
-'sfc': 'snes',
-'smc': 'snes',
-'sms': 'mastersystem',
-
-# same named, all nintendo, interesting:
-'nes': 'nes',
-'snes': 'snes',
-'n64': 'n64',
-'gb': 'gb',
-'gbc': 'gbc',
-'gba': 'gba',
-'fds': 'fds',
-
-# not verified (directory):
-'jag': 'atarijaguar'
-
+    # same named, all nintendo, interesting:
+    'nes': 'nes',
+    'snes': 'snes',
+    'n64': 'n64',
+    'gb': 'gb',
+    'gbc': 'gbc',
+    'gba': 'gba',
+    'fds': 'fds',
 }
 
 
@@ -124,112 +78,42 @@ def file_exists(filename):
     return os.path.exists(filename)
 
 
-def runsh(sh, bufsize=0, shell=True, stdout=None, stderr=None, stdin=None,
-          raise_err=False, duration=None):
-    """
-    This is minirunsh, it runs a shell with the commands given
-
-    sh: a string to execute with a shell
-        example: 'ls -al /tmp'
-
-    bufsize: buffer the output
-             0 = unbuffered (default)
-             -N = system buffering  (-1, etc.)
-             N = lines to buffer (4096, etc.)
-
-    shell: use a login shell to run the commands
-           True = heavier, can use builtins (ls, etc.),
-                  potentially insecure if you use this with a function
-                  that takes in user input!!!!
-
-           False = lighter weight, no builtins, you will probably rarely use
-                   this mode even though its "more secure"
-
-    stdout = where to send stdout, in our case we're going to set it to the
-             pipe from subprocess (as opposed to say, another file handle)
-
-    stderr = where to send stderr - see stdout
-
-    raise_err = raise an error if the returncode is greater than 0 and
-                raise_err is True
-
-    returns a tuple of the subprocesses returncode, stdout and stderr
-    """
-
-    stdout = stdout or subprocess.PIPE
-    stderr = stderr or subprocess.PIPE
-    stdin = stdin or subprocess.PIPE
-    p = subprocess.Popen(args=sh, bufsize=bufsize, shell=shell,
-                         stdout=stdout, stderr=stderr, stdin=stdin)
-
-    if duration:
-        elapsed_time = -1
-        start_time = time.time()
-        while (time.time() - start_time) < duration:
-            rc = p.poll()
-            if rc is not None:
-                elapsed_time = time.time() - start_time
-                break
-            time.sleep(.1)
-
-        if elapsed_time == -1:
-            elapsed_time = duration
-            p.kill()
-    else:
-        start_time = time.time()
-
-    # collect the return values so is "stdout", se is "stderr",
-    # but use non-clobbering variable names (threading future, etc.):
+def runsh(sh):
+    p = subprocess.Popen(args=sh, shell=True,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         stdin=subprocess.PIPE)
     so, se = p.communicate()
-
-    if not duration:
-        elapsed_time = time.time() - start_time
-
-    # rc is "returncode"
     rc = p.returncode
-
-    # raise an error:
-    if rc and raise_err:
-
-        raise ValueError('Returncode[%s] from command: %s' % (rc, sh))
-
-    return (so, se, rc, elapsed_time)
+    return (so, se, rc)
 
 
-def ls_7z(filename):
+def ls_7z(filename, binary):
     """
     use the technical list mode to get the data
     we need
     """
-    if not file_exists(filename):
-        print 'File does not exist: %s' % filename
-        return
-
-    sh = '7z l -slt "%s"' % filename
-    so, se, rc, timed = runsh(sh)
-
-    if rc == 127:
-        print 'Cant find 7z command in path, is it installed?'
-        sys.exit(1)
+    sh = '%s l -slt "%s"' % (binary, filename)
+    so, se, rc = runsh(sh)
 
     file_sizes = {}
 
     if rc == 0:
         candidate = ''
         for line in so.splitlines():
-            if 'Path = ' in line:
-                result = re.search('^Path = (.+)$', line)
+            if 'Path = '.encode(encoding='UTF-8') in line:
+                result = re.search('^Path = (.+)$'.encode(encoding='UTF-8'), line)
                 if result:
                     candidate = result.group(1)
             if candidate:
-                result = re.search('^Size = (.+)$', line)
+                result = re.search('^Size = (.+)$'.encode(encoding='UTF-8'), line)
                 if result:
                     file_sizes[candidate] = result.group(1)
                     candidate = ''
     return file_sizes
 
 
-def extract_7z(filename, target_dir, target=None, overwrite=True):
+def extract_7z(filename, target_dir, binary, target=None, overwrite=True):
     """
     if targets is None, it will extract all files
 
@@ -247,57 +131,55 @@ def extract_7z(filename, target_dir, target=None, overwrite=True):
             backup_name = target_name + '.' + time_str
             #
             shutil.move(target_name, backup_name)
-            print "WARNING!"
-            print "Moved existing: %s" % target_name
-            print "to backup file: %s" % backup_name
-            print ""
+            print()
+            print("Existing File!")
+            print("Existing rom in work directory: %s" % target_name)
+            print("Moved to backup file: %s" % backup_name)
+            print("")
         else:
-            print "Cannot overwrite file %s" % target_name
-            print "exiting"
+            print("Cannot overwrite file %s" % target_name)
+            print("exiting")
             sys.exit(1)
 
-    sh = '7z x "%s" -o"%s" "%s"' % (filename, target_dir, target)
-    so, se, rc, timed = runsh(sh)
+    sh = '%s x "%s" -o"%s" "%s"' % (binary, filename, target_dir, target)
+    so, se, rc = runsh(sh)
 
-    if rc == 127:
-        print 'Cant find 7z command in path, is it installed?'
-        sys.exit(1)
-
-    return so, se, rc, timed
+    return so, se, rc
 
 
-def compress_7z(filename, source):
+def compress_7z(filename, source, binary):
     """
     sources = a tuple of files to add to the archive filename
 
+    theres a bug in here when using 7zr
     """
     if file_exists(filename):
         now = datetime.now()
         time_str = now.strftime("_%Y_%m_%d_%H%M%S")
 
         backup_name = filename + '.' + time_str
-        #
+
         shutil.move(filename, backup_name)
-        print "WARNING!"
-        print "Moved existing: %s" % filename
-        print "to backup file: %s" % backup_name
-        print ""
+        print()
+        print("Existing archive file!")
+        print("Existing archive: %s" % filename)
+        print("Moved to backup: %s" % backup_name)
+        print("")
 
-    sh = '7z a "%s" "%s"' % (filename, source)
-    so, se, rc, timed = runsh(sh)
-
-    if rc == 127:
-        print 'Cant find 7z command in path, is it installed?'
-        sys.exit(1)
-
-    return so, se, rc, timed
+    sh = '%s a "%s" "%s"' % (binary, filename, source)
+    print()
+    print('Compression command used:')
+    print(sh)
+    print()
+    so, se, rc = runsh(sh)
+    return so, se, rc
 
 
 def which_bin(program):
     """
     returns the stdout from "which <program>"
     """
-    so, se, rc, timed = runsh('which %s' % program)
+    so, se, rc = runsh('which %s' % program)
     if not rc:
         retval = so
     else:
@@ -305,16 +187,16 @@ def which_bin(program):
     return retval
 
 
-def draw_menu(archive_menu, regular_menu, rom_key, rom_country):
+def draw_menu(archive_menu, regular_menu, rom_good, rom_country):
 
-    print ""
-
+    print()
+    print("Archive's roms:")
     good_roms = {}
-    for k, v in archive_menu.iteritems():
-        print '%6d: %s' % (k, v)
-        if rom_key in v:
+    for k, v in archive_menu.items():
+        print('%6d: %s' % (k, v.decode('utf-8')))
+        if rom_good.encode(encoding='UTF-8') in v:
             good_roms[k] = v
-    
+
     # needs better first_match logic - Donkey Kong Country.7z shows this,
     # where it has multiple versions but it selects the middle version instead
     # of the later one.  I would like to default to the later one to pickup any
@@ -322,18 +204,24 @@ def draw_menu(archive_menu, regular_menu, rom_key, rom_country):
     # are released.  This might suck for games like punch out on the nes.
     first_match = ''
     if good_roms:
-        print '---'
-        for k,v in good_roms.iteritems():
-            if '(%s)' % rom_country in v:
+        print()
+        print('"Good" choices, %s:' % rom_good)
+        for k,v in good_roms.items():
+            if rom_country.encode(encoding='UTF-8') in v:
                 if not first_match:
-                    first_match = (k,v)
-            
-            print '%6d: %s' % (k, v)
+                    first_match = (k,v.decode('utf-8'))
+            print('%6d: %s' % (k, v.decode('utf-8')))
 
+    if first_match:
+         print('')
+         print('Good choice (%s) + country code (%s):' % (rom_good, rom_country))
+         print('     A: %s' % first_match[1])
 
-    for k, v in regular_menu.iteritems():
-        print '%6s: %s' % (k, v)
-    
+    print()
+    print('Controls:')
+    for k, v in regular_menu.items():
+        print('%6s: %s' % (k, v))
+
     return first_match
 
 
@@ -342,35 +230,34 @@ def main():
     open a specified archive, displaying a menu of its contents.
     offer to write out the contents discretely as new archives and do so.
     """
+    binary = '7z'
+    if not which_bin(binary):
+        binary = '7zr'
+        if not which_bin(binary):
+            print('cannot find a 7zip handler in path, exiting')
+            sys.exit(1)
 
-    if not which_bin('7z'):
-        print 'cannot find 7z in path, exiting'
-        sys.exit(1)
-    
-    
-
+    if len(sys.argv) < 2:
+        raise RuntimeError('Need to specify a rom archive(tarball) to process')
 
     archive_file = sys.argv[1]
-    rom_key = '[!]'
-    rom_country = 'U'
-    archive_list = ls_7z(archive_file)
-    
+    if not file_exists(archive_file):
+        raise RuntimeError('Archive file doesnt exist: %s' % archive_file)
+
+    rom_good = ROM_GOOD
+    rom_country = ROM_COUNTRY
+    archive_list = ls_7z(archive_file, binary)
+
     selection_menu = True
     if len(archive_list) <= 1:
-        print 'Single file in archive, not displaying menu.'
-        print archive_list
-        # some day this might copy it anyways
-        
-        # exit 0 for now so && chaining of the command can take place
-        #sys.exit(0)
+        print('Single file in archive, not displaying menu.')
+        print(archive_list)
         selection_menu = False
         user_choice = 1
         archive_menu = {}
         for key in archive_list:
             archive_menu[1] = key
-        
 
-    
     if selection_menu:
         counter = 1
         archive_menu = {}
@@ -379,86 +266,85 @@ def main():
             counter += 1
 
         regular_menu = {}
+        regular_menu['R'] = 'Redraw the roms menu'
         regular_menu['Q'] = 'Quit the program, back to shell'
-        
-        first_match = draw_menu(archive_menu, regular_menu, rom_key, rom_country)
-        
 
-        
+        first_match = draw_menu(archive_menu, regular_menu, rom_good, rom_country)
+
         user_choice = False
         while not user_choice:
             # user input - which roms to handle?
-            if first_match:
-                 print ''
-                 print '(D)efault: %s' % first_match[1]
-            print ''
-            print 'Which Rom to handle?'
-            user_input = raw_input('')
+            print('')
+            user_input = input('Menu choice: ')
             # scrub user input better
             if re.search('^\d+$', user_input):
                 if int(user_input) >= 1 and int(user_input) <= len(archive_menu):
                     user_choice = user_input
 
-            if user_input == 'q' or user_input == 'Q':
+            if user_input.upper() == 'Q':
                 sys.exit(0)
-            
+
+            if user_input.upper() == 'R':
+                first_match = draw_menu(archive_menu, regular_menu, rom_good, rom_country)
+                print()
+
             if first_match:
-                if user_input == 'd' or user_input == 'D':
+                if user_input.upper() == 'A':
                     user_choice = first_match[0]
-            
+
             if not user_choice:
-                print 'Selection not possible'
-                print ''
-     
+                print('Please select an item before pressing enter')
 
-    picked_file = archive_menu[int(user_choice)]
-    #print picked_file
-    # use filename format?
 
-    so, se, rc, timed = extract_7z(archive_file, WORK_DIR, picked_file)
+    picked_file = archive_menu[int(user_choice)].decode('utf-8')
+
+    so, se, rc = extract_7z(archive_file, WORK_DIR, binary, picked_file)
 
     if rc:
-        print "ERROR! Could not extract file in work directory"
-        print so
-        print se
+        print("ERROR! Could not extract file in work directory %s" % WORK_DIR)
+        print(so)
+        print(se)
         sys.exit(1)
     else:
-        print "Extracted rom %s to %s" % (picked_file, WORK_DIR)
+        print("Extracted rom: %s" % picked_file)
+        print("Work Directory: %s" % WORK_DIR)
 
 
     rom_extension = picked_file.split('.')[-1]
     rom_filename = '.'.join(picked_file.split('.')[:-1])
 
-    new_filename = WORK_DIR + '/' + rom_filename + '.7z'
-    source_filename = WORK_DIR + '/' + picked_file
-    so, se, rc, timed = compress_7z(new_filename, source_filename)
+    new_filename = '%s/%s.7z' % (WORK_DIR, rom_filename)
+    source_filename = '%s/%s' % (WORK_DIR, picked_file)
+    so, se, rc = compress_7z(new_filename, source_filename, binary)
 
     if rc:
-        print "ERROR! Could not save file in work directory"
-        print so
-        print se
+        print("ERROR! Could not save file in work directory")
+        print(so)
+        print(se)
         sys.exit(1)
-    else:
-        print "Compressed rom %s to %s" % (picked_file, new_filename)
 
     if rom_extension in EXTENSIONS:
-        sys_dir = ROMS_DIR + '/' + EXTENSIONS[rom_extension]
+        sys_dir = '%s/%s' % (ROMS_DIR, EXTENSIONS[rom_extension])
     else:
-        print 'Unknown rom type, not copying rom to target directory'
+        print('Unknown rom type, not copying rom to target directory')
         sys.exit(1)
 
-    print 'Sys_dir %s' % sys_dir
     if not file_exists(sys_dir):
-        print "Unable to copy file, directory does not exist: %s" % sys_dir
+        print("Unable to copy file, directory does not exist: %s" % sys_dir)
         sys.exit(1)
 
-    rom_target = sys_dir + '/' + rom_filename + '.7z'
+
+    print("Files/Directories:")
+    print(" - Uncompressed rom: %s/%s" % (WORK_DIR, picked_file))
+    print(" - Archive file: %s/%s.7z" % (sys_dir, rom_filename))
+
+    rom_target = '%s/%s.7z' % (sys_dir, rom_filename)
     if not file_exists(rom_target):
         try:
-            shutil.copy(source_filename, rom_target)
-            print "Copied new archive to %s" % rom_target
+            shutil.copy(new_filename, rom_target)
+            print(" - Copied new archive to %s" % rom_target)
         except IOError as io_error:
-            print "Unable to copy file. %s" % io_error
+            print("!!!!!!! Unable to copy new archive. Error: %s" % io_error)
             sys.exit(1)
 
 
@@ -487,3 +373,23 @@ if __name__ == '__main__':
 # (7z list it? other checks?), then mv it over to
 # the ROMS_DIR
 #
+
+
+"""
+TODO:
+- make file tree menu at start instead of whine about missing file
+
+- loop in the menu so multiple roms can be picked
+
+- multiple roms at once (1,2,3)
+
+- drive new system aliasing, like pcengine = turbografx16, etc.
+  (rom aliasing is implemented, but probably not completely defined)
+
+- ansi/curses menus
+
+- fix pathing so that it can handle various references (./rom.zip, etc.)
+  (workaround is to use full paths)
+
+
+"""
